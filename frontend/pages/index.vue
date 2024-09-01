3d2f49196f8a7f39548d43f9147ba8939c4294ae
VR <rinconvargasvictormanuel@gmail.com>
2024-09-01 09:24:50 -0500

    index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,18 @@
 <template>
-    <div class="flex flex-col justify-center h-full">
-        <div>
-            <div class="UCard mx-auto max-w-3xl shadow-xl py-16 px-4 forced-color-adjust-auto rounded-div"
-                style="background-color: white">
-                <div class="grid grid-cols-2 gap-8">
-                    <div class="py-16 text-center text-gray-400">
-                        <router-link to="/home">
-                            <img src="@/assets/img/GLOBALIPS.png" alt="RIVERA"
-                                class="object-contain w-1/1 md:w-1/1 lg:w-1/1 xl:w-1/1">
-                        </router-link>
-                    </div>
-                    <LoginCard></LoginCard>
-                    <slot></slot>
+<div class="flex flex-col justify-center h-full">
+    <div>
+        <div class="UCard mx-auto max-w-3xl shadow-xl py-16 px-4 forced-color-adjust-auto rounded-div" style="background-color: white">
+            <div class="grid grid-cols-2 gap-8">
+                <div class="py-16 text-center text-gray-400">
+                    
+                    <img src="@/assets/img/GLOBALIPS.png" alt="RIVERA" class="object-contain w-1/1 md:w-1/1 lg:w-1/1 xl:w-1/1">
                 </div>
+                <LoginCard></LoginCard>
+                <slot></slot>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 
@@ -33,7 +30,6 @@ definePageMeta({
 
 <style scoped>
 .rounded-div {
-    border-radius: 50px;
-    /* Ajusta el valor según tus preferencias */
-}
+    border-radius: 50px; /* Ajusta el valor según tus preferencias */
+  }
 </style>
