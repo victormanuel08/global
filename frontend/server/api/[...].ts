1ba0f867d825fe443ebfc4d7cb358d154d9d40d4
Victor M <rinconvargasvictormanuel@gmail.com>
2024-12-13 20:56:01 -0500

    Update API URL to use HTTPS for improved security

--- frontend/server/api/[...].ts
+++ frontend/server/api/[...].ts
@@ -1,6 +1,6 @@
 import type { H3Event, HTTPMethod } from "h3";
 import { FetchError } from "ofetch";
-const API_URL = "http://api.globalsafeips.com.co"
+const API_URL = "https://api.globalsafeips.com.co"
 //const API_URL = "http://localhost:8000"
 
 export default defineEventHandler(async (event) => {
